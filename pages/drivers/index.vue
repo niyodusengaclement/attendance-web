901d480592d3c1aa5c7939a1b00f54c25564fc4a
Dally469 <dallyjones6@gmail.com>
2024-01-20 17:06:37 +0200

    fix bugs

--- pages/drivers/index.vue
+++ pages/drivers/index.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import UiParentCard from '@/components/shared/UiParentCard.vue';
-import { Header } from "vue3-easy-data-table"
+import { Header, Item } from "vue3-easy-data-table"
+import { Field, useValidation } from 'vue3-form-validation';
 
 definePageMeta({
     layout: "admin",
@@ -10,15 +11,87 @@ const http = useHttpRequest()
 const instance = getCurrentInstance();
 const search = ref("");
 const loading = ref(false);
+const isEditing = ref(false);
+const isDeleting = ref(false);
 const lists = ref([]);
+
+interface FormData {
+    names: Field<string>;
+    phone: Field<string>;
+}
+
+const { form, validateFields, resetFields } = useValidation<FormData>({
+    names: {
+        $value: "",
+        $rules: [rules.required("Please name must be provided")],
+    },
+    phone: {
+        $value: "",
+        $rules: [rules.required("Please name must be provided")],
+    },
+});
+
 const headers: Header[] = [
     { text: "ID", value: "id", sortable: true },
     { text: "Names", value: "names", sortable: true },
     { text: "Phone", value: "phone", sortable: true },
     { text: "Type", value: "type", sortable: true },
-    { text: "PLate", value: "plate_number", sortable: true },
+    { text: "Code", value: "code", sortable: true },
     { text: "Actions", value: "actions", width: 120 },
 ]
+const state = ref(1);
+
+
+const editingItem = reactive({
+    names: "",
+    phone: "",
+    code: "",
+    type: "",
+    document: "",
+    id: 0,
+});
+const editItem = (val: Item) => {
+    isEditing.value = true;
+    state.value = 3;
+    const { names, code, phone, type, document, id } = val;
+    editingItem.names = names;
+    editingItem.phone = phone;
+    editingItem.code = code;
+    editingItem.type = type;
+    editingItem.id = id;
+};
+const deleteItem = (val: Item) => {
+    isDeleting.value = true;
+    const { names, id } = val;
+    editingItem.names = names;
+    editingItem.id = id;
+};
+
+async function createDriver() {
+    loading.value = true;
+    var formData = new FormData();
+    formData.append("names", form.names.$value);
+    formData.append("phone", form.phone.$value);
+    formData.append("type", editingItem.type);
+    http
+        .fetch("create_driver", {
+            method: "post",
+            body: formData,
+        })
+        .then((data: any) => {
+            useToast().success(data.message);
+            loadAllDrivers();
+        })
+        .catch((data: any) => {
+            useToast().error(data.data.message);
+            console.log(data);
+        })
+        .finally(() => {
+            loading.value = false;
+            state.value = 1;
+
+        });
+}
 
 
 function loadAllDrivers() {
@@ -36,29 +109,25 @@ function loadAllDrivers() {
 
 const statusStr = (status: string) => {
     if (status == "1") {
-        return "Approved";
-    } else if (status == "2") {
-        return "On Delivery";
-    } else if (status == "3") {
-        return "Arrived";
-    } else if (status == "4") {
-        return "Cancelled";
+        return "Active";
     } else {
-        return "Pending";
+        return "Blocked";
     }
 }
 
 const statusClr = (status: string) => {
     if (status == "1") {
-        return "primary";
-    } else if (status == "2") {
-        return "secondary";
-    } else if (status == "3") {
         return "success";
-    } else if (status == "4") {
-        return "error";
     } else {
         return "warning";
+    }
+}
+
+async function handleSubmit() {
+    try {
+        const formData = await validateFields();
+    } catch (e) {
+        console.log(e);
     }
 }
 onMounted(() => {
@@ -68,8 +137,46 @@ onMounted(() => {
 </script>
 <template>
     <v-row>
-        <v-col cols="12" md="12">
+        <!-- ADD NEW RECORD -->
+        <v-col cols="12" v-show="state == 2" md="4">
+            <UiParentCard :title="'Create Driver'" class="text-success">
+                <form ref="myForm" role="form" @submit.prevent="handleSubmit">
+                    <v-col cols="12">
+
+
+                        <v-text-field variant="outlined" density="compact" label="Name" v-model="form.names.$value"
+                            @blur="form.names.$validate()" color="primary"
+                            :error-messages="form.names.$errors"></v-text-field>
+                        <v-text-field variant="outlined" density="compact" label="Phone" v-model="form.phone.$value"
+                            @blur="form.phone.$validate()" color="primary"
+                            :error-messages="form.phone.$errors"></v-text-field>
+
+                        <v-text-field variant="outlined" density="compact" label="Category" v-model="editingItem.type"
+                            color="primary"></v-text-field>
+
+                        <v-btn :disabled="loading" :loading="loading" @click="createDriver()" class="my-2" color="primary"
+                            size="large" block flat>Save Driver</v-btn>
+                    </v-col>
+                </form>
+            </UiParentCard>
+        </v-col>
+        <v-col cols="12" :md="state == 1 ? '12' : '8'">
             <UiParentCard parent-title="Dashboard" title="Drivers">
+                <v-row class=" flex mb-4 justify-between">
+                    <v-col cols="12" md="8">
+                        <v-text-field v-model="search" :loading="loading" variant="outlined" density="compact"
+                            label="Search for Title or Something" prepend-inner-icon="mdi-magnify" single-line hide-details>
+                        </v-text-field>
+                    </v-col>
+                    <v-col class="flex justify-end" cols="12" md="4">
+                        <v-btn prepend-icon="mdi-vuetify" color="primary" class="mx-2" variant="outlined">
+                            Filters
+                        </v-btn>
+                        <v-btn prepend-icon="mdi-plus" @click="state = 2" color="success" class="mx-2" variant="tonal">
+                            Add New Driver
+                        </v-btn>
+                    </v-col>
+                </v-row>
                 <ClientOnly>
                     <EasyDataTable empty-message="No Order found" :search-value="search" theme-color="#5d87ff"
                         table-class-name="eztable" :headers="headers" buttons-pagination :loading="loading" :items="lists">
