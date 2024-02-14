79da973fec13af06ad812dbb3ca61875c934ce66
bliserge <bliserge34@gmail.com>
2024-02-14 10:52:34 +0200

    bug fix

---
+++ pages/reports/sales.vue
@@ -0,0 +1,114 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import UiParentCard from '@/components/shared/UiParentCard.vue';
+import { Header } from "vue3-easy-data-table"
+
+definePageMeta({
+    layout: "admin",
+});
+const http = useHttpRequest()
+const search = ref("");
+const lists = ref([]);
+const showForm = ref(false)
+const loading = ref(false)
+const headers: Header[] = [
+    { text: "Date", value: "created_at", sortable: true },
+    { text: "Order ID", value: "reference_code", sortable: true },
+    { text: "Client name", value: "customer_name", sortable: true },
+    { text: "Client Phone", value: "phone_number", sortable: true },
+    { text: "Amount", value: "amount_paid", sortable: true },
+    { text: "Shop name", value: "shop_name", sortable: true },
+    { text: "Driver", value: "driver", sortable: true },
+    { text: "Payment Method", value: "payment_mode", sortable: true },
+    { text: "Actions", value: "actions", width: 120 },
+]
+const user = JSON.parse(localStorage.getItem("logger"))
+
+const statusStr = (status: string) => {
+    if (status == "1") {
+        return "Open";
+    } else if (status == "2") {
+        return "On Delivery";
+    } else if (status == "3") {
+        return "Arrived";
+    } else if (status == "4") {
+        return "Cancelled";
+    } else {
+        return "Closed";
+    }
+}
+
+const statusClr = (status: string) => {
+    if (status == "1") {
+        return "success";
+    } else if (status == "2") {
+        return "secondary";
+    } else if (status == "3") {
+        return "success";
+    } else if (status == "4") {
+        return "error";
+    } else {
+        return "error";
+    }
+}
+
+function getSalesReport() {
+    loading.value = true
+    http.fetch("getSalesReport")
+        .then(res => {
+            lists.value = res
+        })
+        .catch(err => {
+            console.log(err.response.message);
+
+        })
+        .finally(() => {
+            loading.value = false
+        })
+}
+
+
+onMounted(() => {
+    getSalesReport()
+})
+
+</script>
+<template>
+    <v-row>
+        <v-col>
+            <UiParentCard parent-title="Dashboard" title="Sales Report">
+                <v-row class="mb-4">
+                    <v-col cols="12" md="6">
+                        <v-text-field v-model="search" variant="outlined" density="compact" label="Search for Title"
+                            prepend-inner-icon="mdi-magnify" single-line hide-details>
+                        </v-text-field>
+                    </v-col>
+                    <v-col class="flex" cols="12" md="2">
+                        <v-btn prepend-icon="mdi-microsoft-excel" color="success" class="mx-2"
+                            variant="tonal">
+                            Export
+                        </v-btn>
+                    </v-col>
+                </v-row>
+                <ClientOnly>
+                    <EasyDataTable empty-message="No Order found" :search-value="search" theme-color="#5d87ff"
+                        table-class-name="eztable" :headers="headers" buttons-pagination :loading="loading" :items="lists">
+                        <template #item-status="item">
+                            <v-chip size="small" :color="statusClr(item.status)"> {{ statusStr(item.status) }} </v-chip>
+                        </template>
+                        <template #item-actions="item">
+                            <v-btn variant="outlined" size="small" color="info"> <v-icon>mdi-view-headline</v-icon> view</v-btn>
+                        </template>
+
+                        <template #empty-message>
+                            <div class="d-flex justify-center align-center py-3">
+                                <v-img src="/images/products/not_found.png" height="150"></v-img>
+                            </div>
+                            <p class="text-muted font-weight-light"> No Found</p>
+                        </template>
+                    </EasyDataTable>
+                </ClientOnly>
+            </UiParentCard>
+        </v-col>
+    </v-row>
+</template>
